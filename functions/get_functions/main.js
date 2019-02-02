0c96c7513faa8c8f89a304bd1f786bea9bd09a65
TPei <t.s.peikert@gmail.com>
2019-02-02 13:58:09 +0100

    add compatibility info, image_url and link to functions

--- functions/get_functions/main.js
+++ functions/get_functions/main.js
@@ -4,17 +4,26 @@ function main(params) {
       {
         name: "anonymizer",
         description: "Replaces data with *",
-        author: "tpei"
+        author: "tpei",
+        compatibility: ["string"],
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png",
+        link: 'https://github.com/TPei/thesis_code/tree/master/functions/transformation_orchestration/anonymizer'
       },
       {
         name: "only_provider",
         description: "return provider.tld from email",
         author: "tpei"
+        compatibility: ["string"],
+        image_url: null,
+        link: null
       },
       {
         name: "none",
         description: "returns passed data (echo)",
         author: "tpei"
+        compatibility: ["json"],
+        image_url: null,
+        link: null
       }
     ]
   }
