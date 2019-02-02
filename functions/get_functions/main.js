1f23256f1b14d8f23478c4538911f1957defab2f
TPei <t.s.peikert@gmail.com>
2019-02-02 09:21:01 +0100

    functions should have authors

--- functions/get_functions/main.js
+++ functions/get_functions/main.js
@@ -1,9 +1,21 @@
 function main(params) {
   return {
     functions: [
-      { name: "anonymizer", description: "Replaces data with *" },
-      { name: "only_provider", description: "return provider.tld from email" },
-      { name: "none", description: "returns passed data (echo)" }
+      {
+        name: "anonymizer",
+        description: "Replaces data with *",
+        author: "tpei"
+      },
+      {
+        name: "only_provider",
+        description: "return provider.tld from email",
+        author: "tpei"
+      },
+      {
+        name: "none",
+        description: "returns passed data (echo)",
+        author: "tpei"
+      }
     ]
   }
 }
