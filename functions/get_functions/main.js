f33cb6f75381524f1c3438cc47a6b561d9c9f62c
TPei <t.s.peikert@gmail.com>
2019-02-04 11:39:14 +0100

    update fetcher function to return data according to new spec

--- functions/get_functions/main.js
+++ functions/get_functions/main.js
@@ -1,29 +1,14 @@
 function main(params) {
   return {
-    functions: [
+    "functions": [
       {
-        name: "anonymizer",
-        description: "Replaces data with *",
-        author: "tpei",
-        compatibility: ["string"],
-        image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png",
-        link: 'https://github.com/TPei/thesis_code/tree/master/functions/transformation_orchestration/anonymizer'
-      },
-      {
-        name: "only_provider",
-        description: "return provider.tld from email",
-        author: "tpei"
-        compatibility: ["string"],
-        image_url: null,
-        link: null
-      },
-      {
-        name: "none",
-        description: "returns passed data (echo)",
-        author: "tpei"
-        compatibility: ["json"],
-        image_url: null,
-        link: null
+        "name": "anonymizer",
+        "author": "tpei",
+        "description": "This function replaces all string's characters with *",
+        "compatibility": ["string"],
+        "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png",
+        "repo_link": "https://github.com/TPei/yappl_transformation_functions/tree/master/functions/anonymizer",
+        "code_link": "https://raw.githubusercontent.com/TPei/yappl_transformation_functions/master/functions/anonymizer/anonymizer.js"
       }
     ]
   }
