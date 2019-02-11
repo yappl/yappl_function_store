1ecd786ad0f1786c67ae2ffb2e4d9d98764fe27e
TPei <t.s.peikert@gmail.com>
2019-02-11 17:38:02 +0100

    implement get command

--- cli/lib/commands/get_command.cr
+++ cli/lib/commands/get_command.cr
@@ -1,11 +1,43 @@
 class GetCommand
   def run(args = [] of String)
-    if args.size > 0 && ["--help", "-h"].includes? args[0]
-      puts help
+    arguments = ArgumentParser.parse(args_map, args)
+    return puts help if arguments.has_key?("-h") || arguments.has_key?("--help")
+
+    function_name = args[0]
+
+    function = FunctionsWrapper.function_by(function_name)
+
+    if function.nil?
+      puts "no function by that name found"
       return
     end
 
-    puts "getting functions from #{URL}"
+    function_url = function.code_link
+    response = HTTP::Client.get(function_url).body
+
+    filename = function_url.split("/")[-1]
+    if File.exists?(filename)
+      puts "file by that name already exists"
+      return
+    else
+      File.write(filename, response)
+    end
+
+    if File.exists?("manifest.yml")
+      puts "manifest file already exists"
+      return
+    else
+      manifest = <<-HEREDOC
+      packages:
+        transformations:
+          actions:
+            #{function_name}:
+              function: ./#{filename}
+      HEREDOC
+      File.write("manifest.yml", manifest)
+    end
+
+    puts "Downloaded #{filename} to local directory"
   end
 
   def help
@@ -22,5 +54,9 @@ class GetCommand
     Flags:
       -h, --help   help for show command
     HEREDOC
+  end
+
+  def args_map
+    { "--help" => 0, "-h" => 0 }
   end
 end
