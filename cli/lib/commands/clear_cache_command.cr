fb2f5423c92d77eb354322c3ddf18a1b44f04b57
TPei <t.s.peikert@gmail.com>
2019-02-11 18:03:48 +0100

    add help flag to cache clear command

--- cli/lib/commands/clear_cache_command.cr
+++ cli/lib/commands/clear_cache_command.cr
@@ -1,6 +1,29 @@
 class ClearCacheCommand
   def run(args = [] of String)
+    arguments = ArgumentParser.parse(args_map, args)
+    return puts help if arguments.has_key?("-h") || arguments.has_key?("--help")
+
     Cache.new(URL).clear
     puts "Cache Cleared"
+  end
+
+  def help
+    return <<-HEREDOC
+
+    Clears Function Cache. Deletes all cached data for the currently used function store.
+
+    Usage:
+      yt clear
+
+    Flags:
+      -h, --help      help for deploy command
+    HEREDOC
+  end
+
+  def args_map
+    {
+      "-h" => 0,
+      "--help" => 0,
+    }
   end
 end
