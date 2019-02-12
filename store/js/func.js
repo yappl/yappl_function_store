1466c309bff975f478baa06b32b4ee9abc631d08
TPei <t.s.peikert@gmail.com>
2019-02-12 15:36:26 +0100

    update Func to proper es6 class definition

--- store/js/func.js
+++ store/js/func.js
@@ -1,29 +1,24 @@
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
+class Func {
+  constructor(func) {
+    this.name = func.name;
+    this.description = func.description;
+    this.author = func.author;
+    if(this.author == undefined || this.author == "")
+      this.author = 'unknown'
 
-var Func = function Func(func) {
-  _classCallCheck(this, Func);
-  this.name = func.name;
-  this.description = func.description;
-  this.author = func.author;
-  if(this.author == undefined || this.author == "")
-    this.author = 'unknown'
+    this.compatibility = func.compatibility
+    this.runtime = func.runtime
 
-  this.compatibility = func.compatibility
-  this.runtime = func.runtime
-
-  this.image_url = func.image_url
-  if(this.image_url == undefined || this.image_url == "") {
-    if (Math.random() > 0.5) {
-      this.image_url = './images/cloud_function.png'
-    } else {
-      this.image_url = './images/openwhisk.jpg'
+    this.image_url = func.image_url
+    if(this.image_url == undefined || this.image_url == "") {
+      if (Math.random() > 0.5) {
+        this.image_url = './images/cloud_function.png'
+      } else {
+        this.image_url = './images/openwhisk.jpg'
+      }
     }
-  }
 
-  this.repo_link = func.repo_link
-  this.code_link = func.code_link
+    this.repo_link = func.repo_link
+    this.code_link = func.code_link
+  }
 }
