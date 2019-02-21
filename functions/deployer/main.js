246adb8ce13bbdbd14e4332014ef1dde782ef6f1
TPei <t.s.peikert@gmail.com>
2019-02-21 16:03:48 +0100

    deployer function cleanup

--- functions/deployer/main.js
+++ functions/deployer/main.js
@@ -1,5 +1,4 @@
 const openwhisk = require('openwhisk')
-const https = require('https');
 
 function main(params) {
   const ow = openwhisk();
@@ -15,28 +14,4 @@ function main(params) {
     "result": true,
     "blocking": true
   });
-
-  //https.get(params.url, (res) => {
-    //console.log('statusCode:', res.statusCode);
-    //console.log('headers:', res.headers);
-
-    //res.setEncoding('utf8');
-    //let rawData = '';
-    //res.on('data', (chunk) => { rawData += chunk; });
-    //res.on('end', () => {
-      //console.log('data:', rawData);
-      //ow.actions.create({name, rawData}).then(result => {
-        //console.log('action created!', name)
-      //}).catch(err => {
-        //console.error('failed to create action', err)
-      //});
-    //});
-  //}).on('error', (e) => {
-    //console.error(e);
-  //});
-
-  return { success: true }
 }
-
-//console.log('-----------')
-//console.log(main({ name: 'anonymizer', url: 'https://raw.githubusercontent.com/TPei/yappl_transformation_functions/master/functions/anonymizer/anonymizer.js' }))
